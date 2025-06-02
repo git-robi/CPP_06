706b6b3e6479f042ecf2fe117e462196501599e3
Roberta Giambona <rgiambon@car14s2.42barcelona.com>
2025-06-02 13:34:18 +0200

    fix: uintptr

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rgiambon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 12:46:09 by rgiambon          #+#    #+#             */
-/*   Updated: 2025/06/02 12:46:11 by rgiambon         ###   ########.fr       */
+/*   Updated: 2025/06/02 13:33:54 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 int main()
 {
     Data* data = new Data;
-    data->number = 42;
+	data->number = 42;
     data->text = "Hello, World!";
 
-    size_t serialized = Serializer::serialize(data);
+    uintptr_t serialized = Serializer::serialize(data);
     Data* deserialized = Serializer::deserialize(serialized);
 
-  
+
     std::cout << "Original pointer: " << data << std::endl;
     std::cout << "Deserialized pointer: " << deserialized << std::endl;
     std::cout << "Are equal: " << (data == deserialized) << std::endl;
